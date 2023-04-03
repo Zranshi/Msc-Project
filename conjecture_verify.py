b1af1926773961fdc6dfefddbf6fc57d725acb60
Ranshi <ranshi159@icloud.com>
2023-04-03 10:13:30 +0100

    2023.4.3

--- conjecture_verify.py
+++ conjecture_verify.py
@@ -1,5 +1,7 @@
 # conjecture: For each operator, some kind of transformation is performed on it, which can obtain the same number of states.
-# That is, suppose there is such an operator p, 0 p 0 = 1, 0 p 2 = 0, and all others are 2. Then there is an operator q, and 0 and 1 in all operation rules of q are swapped. Then we can get The specific operation rules of q: 1 p 1 = 0, 1 p 2 = 1, and all other operations are 2.
+# That is, suppose there is such an operator p, 0 p 0 = 1, 0 p 2 = 0, and all others are 2.
+# Then there is an operator q, and 0 and 1 in all operation rules of q are swapped.
+# Then we can get The specific operation rules of q: 1 p 1 = 0, 1 p 2 = 1, and all other operations are 2.
 # Moreover, the number of states that p and q can obtain from the initial state is the same.
 import itertools
 
@@ -57,4 +59,5 @@ if __name__ == "__main__":
 # Misconjecture: 0,0:1 0,1:0 0,2:2 1,0:2 1,1:2 1,2:0 2,0:1 2,1:2 2,2:1, 1,1:2 1,2:1 1,0:0 2,1:0 2,2:0 2,0:1 0,1:2 0,2:0 0,0:2
 
 
-# Analysis: For most of the results, it is valid, but there are a small number of failures. I don't know what the reason is. It may be that the conjecture is wrong, or there is an error in the result of NAND-like-operator.
+# Analysis: For most of the results, it is valid, but there are a small number of failures.
+# I don't know what the reason is. It may be that the conjecture is wrong, or there is an error in the result of NAND-like-operator.
